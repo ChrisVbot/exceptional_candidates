6b3ecb3b69d371847688e66ecd5fbed7275b98c7
ChrisVbot <chris.veillette@gmail.com>
2016-07-22 20:11:05 +0000

    Final except color

--- filters.rb
+++ filters.rb
@@ -1,11 +1,65 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
-
+require 'pry'
+#Takes in a hash representing a candidate ID and returns that ID
 def find(id)
-
+  id[:id] 
 end
 
+#Takes in a hash representing a candidate and returns whether candidate 
+#  has 2 years experience or more as boolean
 def experienced?(candidate)
+  candidate[:years_of_experience] >= 2
+end    
+
+#Qualifies all candidates based on having greater than 100 github points
+def github(candidate)
+  candidate[:github_points] >= 100
 end
 
-# More methods will go below
+#Checks hash to verify if candidate knows at least Ruby or Python
+def languages(candidate)
+  candidate[:languages].include?('Python') ||
+  candidate[:languages].include?('Ruby')
+end
+
+#Checks application date of applicant
+def applydate(candidate)
+  candidate[:date_applied] >= 15.days.ago.to_date
+end
+
+#Checks if age of applicant is over 17
+def applyage(candidate)
+  candidate[:age] > 17
+end
+
+#Takes in collection of candidates, returns a subset of candidates that meet certain criteria
+def qualified_candidates(candidates)
+  qualified_candidates = []
+  candidates.each do |candidate|
+    if experienced?(candidate) && github(candidate) && languages(candidate) && applydate(candidate) && applyage(candidate)
+      qualified_candidates << candidate
+    end
+  end
+  return qualified_candidates
+end
+
+#Returns back all candidates but reordered such that candidates with most experience are on top
+#  If candidates have same number of years experience, further sorted by their number of Github points
+def ordered_by_qualifications(candidates)
+  ordered_by_qualification = [] #creates an empty array for the candidates
+  years_of_experience = candidates.sort { |a, b| [a[:years_of_experience], a[:github_points]] <=> [b[:years_of_experience], b[:github_points]] }
+  ordered_by_qualification << years_of_experience.reverse
+  return ordered_by_qualification
+end
+
+
+ 
+
+
+
+
+
+
+
+
--- main.rb
+++ main.rb
@@ -4,9 +4,99 @@
 require 'pry'
 require './candidates'
 require './filters'
+require 'colorize'
 
-## Your test code can go here
 
+def get_input
+  while true
+    puts "Enter command"
+    input = gets.strip
+    return input
+  end
+end
+
+def candidates(candidates)
+  case candidates
+    
+    when 'find 5'
+      output = @candidates[0]
+    
+    when 'find 7'
+      output = @candidates[1]
+
+    when 'find 9'
+      output = @candidates[2]
+
+    when 'find 10'
+      output = @candidates[3]
+
+    when 'find 11'
+      output = @candidates[4]
+
+    when 'find 15'
+      output = @candidates[5]  
+
+    when 'quit'
+      puts 'Exiting program'
+      exit
+    
+    when 'qualified'
+      output = qualified_candidates(@candidates).join("\n")
+
+    when 'all'
+      output = @candidates.join("\n")
+    
+    else
+      output = "Command or ID not recognized"
+  end
+  puts "#{output}"
+end
+
+def user_entry
+  while true
+    input = get_input
+    output = candidates(input)
+    #puts output
+  end
+end
+
+user_entry
+# Your test code can go here
 # binding.pry
+# pp qualified_candidates
+# @candidates.each do |candidate|
+#   puts experienced?(candidate)
+# end
 
-pp qualified_candidates
+# @candidates.each do |id|
+#   puts find(id)
+# end
+
+# @candidates.each do |github_points|
+#   puts github(github_points)
+# end
+
+# @candidates.each do |langs|
+#   puts languages(langs)
+# end
+
+# @candidates.each do |date|
+#   puts applydate(date)
+# end
+
+# @candidates.each do |ages|
+#   puts applyage(ages)
+# end
+
+# @candidates.each do |qual|
+#  puts qualified_candidates(qual)
+# end
+
+# @candidates.each do |qualified|
+#   puts qualified_candidates(qualified)
+# end
+
+# puts qualified_candidates(@candidates)
+
+
+# puts @candidates
